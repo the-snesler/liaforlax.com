8c9201a09de1fc23fab582e7798e8c56e65e9623
Sam N <78718829+TetraTsunami@users.noreply.github.com>
2026-03-16 13:49:13 -0500

    Run prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,38 @@
 // @ts-check
-import { defineConfig, envField, fontProviders } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig, envField, fontProviders } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
 
   experimental: {
     fonts: [
       {
         provider: fontProviders.google(),
-        name: "Prompt",
-        cssVariable: "--font-prompt",
-        weights: ["400", "700", "900"],
-      },
-    ],
+        name: 'Prompt',
+        cssVariable: '--font-prompt',
+        weights: ['400', '700', '900']
+      }
+    ]
   },
 
   image: {
-    domains: ["images.ctfassets.net"],
+    domains: ['images.ctfassets.net']
   },
 
   env: {
     schema: {
-      GOOGLE_APPS_SCRIPT_URL: envField.string({context: "server", access: "secret"}),
+      GOOGLE_APPS_SCRIPT_URL: envField.string({ context: 'server', access: 'secret' })
     }
   },
 
-  integrations: [icon()],
+  integrations: [icon()]
 });
